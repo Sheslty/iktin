93cd7bc97e9ea4ba8c821b526dde3bb3231a7025
evarava <evarava@sfedu.ru>
2023-10-21 16:18:07 +0300

    FIX: removed redundant code

--- dbcontroller/dbcontroller.py
+++ dbcontroller/dbcontroller.py
@@ -11,19 +11,14 @@ db = SqliteDatabase(config['db_file'])
 
 
 class DataBaseController:
-    def __init__(self, _db):
-        self._db = _db
 
-    def __enter__(self):
-        self._db.connect()
-        return self
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        self._db.close()
-
-    def init(self):
-        self._db.create_tables([UserAccount, TgUserAccount,
-                               Order, Manager, Pretension])
+    @staticmethod
+    def init():
+        UserAccount.create_table()
+        TgUserAccount.create_table()
+        Order.create_table()
+        Manager.create_table()
+        Pretension.create_table()
 
     @staticmethod
     def create(model: type(Model), **kwargs) -> Model:
@@ -97,16 +92,16 @@ class Pretension(Model):
 
 
 if __name__ == '__main__':
-    with DataBaseController(db) as controller:
-        order = controller.create(Order, name='name', info='info')
-        manager = controller.create(Manager, tg_id=1, tg_username='name',
-                                    password='pass')
-        user = controller.create(UserAccount, mail='mail', password='pass',
-                                 contract_number=1, order_id=order.id)
-        controller.create(TgUserAccount, tg_id=1, tg_username='name',
-                          account_id=user.id, manager_id=manager.id)
-        controller.init()
-        user_ids = controller.get_user_ids()
-        print(user_ids)
-        manager_ids = controller.get_manager_ids()
-        print(manager_ids)
+    controller = DataBaseController()
+    order = controller.create(Order, name='name1', info='info1')
+    manager = controller.create(Manager, tg_id=4, tg_username='name1',
+                                password='pass1')
+    user = controller.create(UserAccount, mail='mail1', password='pass1',
+                             contract_number=4, order_id=order.id)
+    controller.create(TgUserAccount, tg_id=5, tg_username='name1',
+                      account_id=user.id, manager_id=manager.id)
+    controller.init()
+    user_ids = controller.get_user_ids()
+    print(user_ids)
+    manager_ids = controller.get_manager_ids()
+    print(manager_ids)
