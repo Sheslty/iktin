22bde379ea2118bf0597c7946dd675ef9a9420a2
Danil <ugarovd06@gmail.com>
2023-10-22 04:10:54 +0300

    create handler for get info order

--- bot/handlers/users.py
+++ bot/handlers/users.py
@@ -1,22 +1,19 @@
 from aiogram import Router, F, types
 from aiogram.types import Message
 from aiogram.fsm.context import FSMContext
-from aiogram.filters.state import State, StatesGroup
-
-from datetime import datetime
+import bot.keyboards as keyboards
 
 from bot.messages import BotButtons
 from datatypes import Package, Item, PretensionStatus
-from dbcontroller.models import TgPretension
+from dbcontroller.models import TgPretension, TgUserAccount, Order
 import bot.keyboards as keyboards
-from keyboards import PretensionCallbacks
+from bot.keyboards import PretensionCallbacks
+from aiogram_inline_paginations.paginator import Paginator
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+from aiogram.filters.callback_data import CallbackData
+
 
 router = Router()
-
-
-# --- Start Consignment create section
-class PretensionCreateForm(StatesGroup):
-    waiting_for_pretension_info = State()
 
 
 @router.message(F.text == BotButtons.PRETENSION_CREATE)
@@ -25,43 +22,26 @@ async def process_pretension_create(message: Message):
     await message.answer("Выберите причину претензии", reply_markup=keyboard)
 
 
-@router.callback_query(F.data.startswith('pretension_'))
-async def pretension_type_choose(callback: types.CallbackQuery, state: FSMContext):
-    pretension_type = None
-    if PretensionCallbacks.PRETENSION_TERM in callback.data:
-        pretension_type = "Нарушение сроков доставки"
-    elif PretensionCallbacks.PRETENSION_BROKE_ITEM in callback.data:
-        pretension_type = "Порча вложения"
-    elif PretensionCallbacks.PRETENSION_LOST_ITEM in callback.data:
-        pretension_type = "Утеря вложения"
-    elif PretensionCallbacks.PRETENSION_BROKE_ITEM in callback.data:
-        pretension_type = "Повреждение упаковки"
-
-    await state.update_data(pretension_type=pretension_type)
-    await callback.message.answer('Опишите вашу претензию подробнее')
-    await state.set_state(PretensionCreateForm.waiting_for_pretension_info)
-
-
-@router.message(PretensionCreateForm.waiting_for_pretension_info)
-async def pretension_info_chosen(message: Message, state: FSMContext):
-    managers_data = await state.get_data()
-    pretension_type = managers_data['pretension_type']
-    pretension_message = message.text
-
-    TgPretension.create(
-        user_id=message.from_user.id, status=PretensionStatus.WAIT, _type=pretension_type,
-        message=pretension_message, creation_datetime=datetime.now()
-    )
-
-    await message.answer("Ваша претензия записана и в ближайшее время будет рассмотрена менеджером.")
-# --- End Consignment create section
-
-
-# --- Start Invoice create section
 @router.message(F.text == BotButtons.CREATE_INVOICE)
 async def process_create_invoice(message: Message, state: FSMContext):
     keyboard = keyboards.delivery_type_choose
-    await message.answer("Выберите режим отправки", reply_markup=keyboard, state=state)
+    await message.answer("Выберите режим отправки", reply_markup=keyboard,
+                         state=state)
+
+
+@router.callback_query(F.data.startswith('cons_term'))
+async def callbacks_num(callback: types.CallbackQuery):
+    action = callback.data.split("_")[1]
+    if action == "term":
+        await callback.message.edit_text(f"Бог поможет")
+    elif action == "broke_item":
+        await callback.message.edit_text(f"Бог поможет")
+    elif action == "lost_item":
+        await callback.message.edit_text(f"Бог поможет")
+    elif action == "broke_box":
+        await callback.message.edit_text(f"Бог поможет")
+
+    await callback.answer()
 
 
 @router.callback_query(F.data.startswith('deliver'))
@@ -84,7 +64,7 @@ async def delivery_type_callback(callback: types.CallbackQuery,
 
 @router.callback_query(F.data == 'packages_number')
 async def packages_number_callback(callback: types.CallbackQuery,
-                                 state: FSMContext):
+                                   state: FSMContext):
     try:
         packages_number = int(callback.message.text)
         packages = [Package(None, []) for _ in range(packages_number)]
@@ -121,7 +101,25 @@ async def packages_number_callback(callback: types.CallbackQuery,
 #                                   reply_markup=buttons, state=state)
 # --- End Invoice create section
 
+class OrderCallbackFactory(CallbackData, prefix='orders'):
+    name: str
+
+
 # TODO: отслеживание заказа
-@router.message(F.text == BotButtons.PRETENSION_CREATE)
+@router.message(F.text == BotButtons.CARGO_TRACKING)
 async def process_cargo_tracking(message: Message, state: FSMContext):
-    await message.reply(f"")
+    user_id = TgUserAccount.get(tg_id=message.from_user.id).user_id
+    order_list = Order.select().where(Order.user_id == user_id)
+    builder = InlineKeyboardBuilder()
+
+    for el in order_list:
+        builder.button(text=f"№{el.id}, '{el.name[:20]}'",
+                       callback_data=OrderCallbackFactory(name=el.name[:20]))
+
+    builder.adjust(1)
+    paginator = Paginator(data=builder.as_markup(), size=5, dp=router)
+
+    await message.answer(
+        text='Выберете заказ:',
+        reply_markup=paginator()
+    )
