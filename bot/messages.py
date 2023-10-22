7ded3f7549eaa90aabaddc1cec60e8f0ed437f00
Danil <ugarovd06@gmail.com>
2023-10-22 11:58:59 +0300

    Add buttons

--- bot/messages.py
+++ bot/messages.py
@@ -11,8 +11,10 @@ class BotButtons:
     AUTHORISE_AS_MANAGER = "Login as manager"
 
     PRETENSION_CREATE = "Create pretension"
+    PRETENSIONS_PROCESSING = "Pretensions processing"
 
     CARGO_TRACKING = "Track cargo"
     CREATE_INVOICE = "Create invoice"
+    START_CHAT_MANAGER = "Start chat with manager"
 
     GET_USERS_FOR_MANAGER = "My clients"
